f93ae63cc8bc443086d0cd9d61a0318f5a461dc5
srikanth <srikanthaddanki222@gmail.com>
2024-02-24 23:50:47 +0530

    . fix indent

--- Blind75/src/main/java/org/example/RotateLeftTheValue/RotateLeft.java
+++ Blind75/src/main/java/org/example/RotateLeftTheValue/RotateLeft.java
@@ -4,18 +4,19 @@ import java.util.Arrays;
 
 public class RotateLeft {
 
-    public static int[] getLeftRotate(int [] arr){
+    public static int[] getLeftRotate(int[] arr) {
         int temp = arr[0];
 
-        for(int i=1; i<arr.length; i++){
-            arr[i-1] = arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            arr[i - 1] = arr[i];
         }
-        arr[arr.length-1] = temp;
+        arr[arr.length - 1] = temp;
         return arr;
     }
+
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int [] value = getLeftRotate(arr);
+        int[] arr = {1, 2, 3, 4, 5};
+        int[] value = getLeftRotate(arr);
         System.out.println(Arrays.toString(value));
     }
 }
